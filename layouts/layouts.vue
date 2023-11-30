02b2f63ac4350379b65586dd9284235397b55c98
MuhammadZulfaKamal31 <zulfakamal019@gmail.com>
2023-11-30 07:21:16 +0700

    yok bisa yok

--- layouts/layouts.vue
+++ layouts/layouts.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="w-full h-full flex" v-if="cekToken">
-        <!-- :class="{
-            ' md:ml-[200px] lg:ml-[300px] w-full duration-300': openSideBar.openSideBar,
-            'w-full duration-300': !openSideBar.openSideBar
-        }" -->
-        <div :class="openSideBar.openSideBar ? ' md:ml-[200px] lg:ml-[300px] w-full duration-300' : 'w-full duration-300'">
-            <div class=" hidden md:block">
-                <div class="  md:flex top-36 flex flex-row absolute justify-center px-50 px-5" :class="{
-                    'ml-[2%] md:w-[70%] lg:w-[69%] xl:w-[74%] duration-300': openSideBar.openSideBar,
-                    ' ml-[5%] w-[90%] duration-300': !openSideBar.openSideBar
-                }">
-                    <div
-                        class="bg-white shadow-xl w-full md:h-16 lg:h-20 rounded-xl flex flex-row justify-between items-center px-5">
-                        <h2 class="text-2xl font-bold">{{ activeLink }}</h2>
-                        <div class="flex flex-row space-x-2 font-semibold text-sm text-red-500">
-                            <div v-for="(link, index) in links " :key="index">
-                                <nuxt-link :to="generateLink(index)"
-                                    class=" hover:text-gray-800 hover:bg-gray-600 hover:bg-opacity-10 p-1 rounded-sm">
-                                    {{ capitalizeFirstLetter(link) }}
-                                </nuxt-link>
-                                <span v-if="!(link === links[links.length - 1])" class=" ml-1">/</span>
+    <div>
+        <!-- v-if="cekToken" -->
+        <div class="w-full h-full flex">
+            <div
+                :class="openSideBar.openSideBar ? ' md:ml-[200px] lg:ml-[300px] w-full duration-300' : 'w-full duration-300'">
+                <div class=" hidden md:block">
+                    <div class="  md:flex top-36 flex flex-row absolute justify-center px-50 px-5" :class="{
+                        'ml-[2%] md:w-[70%] lg:w-[69%] xl:w-[74%] duration-300': openSideBar.openSideBar,
+                        ' ml-[5%] w-[90%] duration-300': !openSideBar.openSideBar
+                    }">
+                        <div
+                            class="bg-white shadow-xl w-full md:h-16 lg:h-20 rounded-xl flex flex-row justify-between items-center px-5">
+                            <h2 class="text-2xl font-bold">{{ activeLink }}</h2>
+                            <div class="flex flex-row space-x-2 font-semibold text-sm text-red-500">
+                                <div v-for="(link, index) in links " :key="index">
+                                    <nuxt-link :to="generateLink(index)"
+                                        class=" hover:text-gray-800 hover:bg-gray-600 hover:bg-opacity-10 p-1 rounded-sm">
+                                        {{ capitalizeFirstLetter(link) }}
+                                    </nuxt-link>
+                                    <span v-if="!(link === links[links.length - 1])" class=" ml-1">/</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- :openSideBar="openSideBar.openSideBar" -->
-            <!-- :class="openSideBar.openSideBar ? 'w-[1135px] duration-300' : 'w-[1425px] duration-300'" -->
-            <TopBar class=" hidden md:block" :toggleSideBar="openSideBar.toggleSidebar" />
-            <TopBarResponsif :toggleSideBar="openSideBar.toggleSidebar" :openSideBar="openSideBar.openSideBar"
-                class=" md:hidden "></TopBarResponsif>
+                <!-- :openSideBar="openSideBar.openSideBar" -->
+                <!-- :class="openSideBar.openSideBar ? 'w-[1135px] duration-300' : 'w-[1425px] duration-300'" -->
+                <TopBar class=" hidden md:block" :toggleSideBar="openSideBar.toggleSidebar" />
+                <TopBarResponsif :toggleSideBar="openSideBar.toggleSidebar" :openSideBar="openSideBar.openSideBar"
+                    class=" md:hidden "></TopBarResponsif>
 
-            <div @click="toggleCloseBar">
-                <router-view></router-view>
+                <div @click="toggleCloseBar">
+                    <router-view></router-view>
+                </div>
             </div>
+            <Sidebar :dataOpenSideBar="openSideBar.openSideBar" class="z-10">
+            </Sidebar>
         </div>
-        <Sidebar :dataOpenSideBar="openSideBar.openSideBar" class="z-10">
-        </Sidebar>
     </div>
 </template>
 
